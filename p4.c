ea7bf7403859956c27c9a120efdb9d5bcce69333
Tejas M <97739480+playgms@users.noreply.github.com>
2024-06-20 11:50:56 +0530

    Update p4.c

--- p4.c
+++ p4.c
@@ -2,152 +2,59 @@
 #include <string.h>
 #include <ctype.h>
 
-int mod26(int a) {
-    return (a % 26 + 26) % 26;
-}
-
-void matrixMultiply(int a[3][3], int b[3], int result[3]) {
-    for (int i = 0; i < 3; i++) {
-        result[i] = 0;
-        for (int j = 0; j < 3; j++) {
-            result[i] += a[i][j] * b[j];
-        }
-        result[i] = mod26(result[i]);
+void toUpperCase(char *str) {
+    for (int i = 0; i < strlen(str); i++) {
+        str[i] = toupper(str[i]);
     }
-}
-
-void getCofactor(int matrix[3][3], int temp[3][3], int p, int q, int n) {
-    int i = 0, j = 0;
-    for (int row = 0; row < n; row++) {
-        for (int col = 0; col < n; col++) {
-            if (row != p && col != q) {
-                temp[i][j++] = matrix[row][col];
-                if (j == n - 1) {
-                    j = 0;
-                    i++;
-                }
-            }
-        }
-    }
-}
-
-int determinant(int matrix[3][3], int n) {
-    int D = 0;
-    if (n == 1) {
-        return matrix[0][0];
-    }
-    int temp[3][3];
-    int sign = 1;
-    for (int f = 0; f < n; f++) {
-        getCofactor(matrix, temp, 0, f, n);
-        D += sign * matrix[0][f] * determinant(temp, n - 1);
-        sign = -sign;
-    }
-    return D;
-}
-
-void adjoint(int matrix[3][3], int adj[3][3]) {
-    if (3 == 1) {
-        adj[0][0] = 1;
-        return;
-    }
-    int sign = 1, temp[3][3];
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            getCofactor(matrix, temp, i, j, 3);
-            sign = ((i + j) % 2 == 0) ? 1 : -1;
-            adj[j][i] = (sign) * (determinant(temp, 3 - 1));
-        }
-    }
-}
-
-int modInverse(int a, int m) {
-    a = a % m;
-    for (int x = 1; x < m; x++) {
-        if ((a * x) % m == 1) {
-            return x;
-        }
-    }
-    return -1;
-}
-
-void findInverse(int matrix[3][3], int inverse[3][3]) {
-    int det = determinant(matrix, 3);
-    det = mod26(det);
-    if (gcd(det, 26) != 1) {
-        printf("Error: Key matrix is not invertible.\n");
-        return;
-    }
-    int adj[3][3];
-    adjoint(matrix, adj);
-    int invDet = modInverse(det, 26);
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            inverse[i][j] = mod26(adj[i][j] * invDet);
-        }
-    }
-}
-
-int gcd(int a, int b) {
-    if (b == 0) {
-        return a;
-    }
-    return gcd(b, a % b);
 }
 
 int main() {
-    int key[3][3];
-    char message[100], encryptedMessage[100], decryptedMessage[100];
-    int i, j, t;
-    unsigned int c[3], d[3];
+    unsigned int encryptionMatrix[3][3] = {{6, 24, 1}, {13, 16, 10}, {20, 17, 15}};
+    unsigned int decryptionMatrix[3][3] = {{8, 5, 10}, {21, 8, 21}, {21, 12, 8}};
+    
+    unsigned int plaintextVector[3], ciphertextVector[3];
+    char plaintext[20];
+    int i, j, temp;
+    
+    printf("Enter the plaintext (3 characters): \n");
+    scanf("%s", plaintext);
 
-    printf("Enter the 3x3 key matrix (9 integers between 0-25):\n");
+    toUpperCase(plaintext);
+    
+    if (strlen(plaintext) != 3) {
+        printf("Error: Plaintext must be exactly 3 characters long.\n");
+        return 1;
+    }
+
     for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            scanf("%d", &key[i][j]);
-        }
+        plaintextVector[i] = plaintext[i] - 'A';
     }
 
-    printf("Enter the plain text (in capitals): ");
-    scanf("%s", message);
-
-    int messageLength = strlen(message);
-    while (messageLength % 3 != 0) {
-        message[messageLength++] = 'X';
+    for (i = 0; i < 3; i++) {
+        temp = 0;
+        for (j = 0; j < 3; j++) {
+            temp += encryptionMatrix[i][j] * plaintextVector[j];
+        }
+        ciphertextVector[i] = temp % 26;
     }
-    message[messageLength] = '\0';
 
-    printf("Original Message: %s\n", message);
-
-    // Encrypt the message
-    for (i = 0; i < messageLength; i += 3) {
-        for (j = 0; j < 3; j++) {
-            c[j] = toupper(message[i + j]) - 65;
-        }
-        matrixMultiply(key, c, d);
-        for (j = 0; j < 3; j++) {
-            encryptedMessage[i + j] = d[j] + 65;
-        }
+    printf("\nEncrypted ciphertext: ");
+    for (i = 0; i < 3; i++) {
+        printf("%c", ciphertextVector[i] + 'A');
     }
-    encryptedMessage[messageLength] = '\0';
-    printf("Encrypted Message: %s\n", encryptedMessage);
 
-    // Calculate the inverse key matrix
-    int inverseKey[3][3];
-    findInverse(key, inverseKey);
-
-    // Decrypt the message
-    for (i = 0; i < messageLength; i += 3) {
+    for (i = 0; i < 3; i++) {
+        temp = 0;
         for (j = 0; j < 3; j++) {
-            d[j] = encryptedMessage[i + j] - 65;
+            temp += decryptionMatrix[i][j] * ciphertextVector[j];
         }
-        matrixMultiply(inverseKey, d, c);
-        for (j = 0; j < 3; j++) {
-            decryptedMessage[i + j] = c[j] + 65;
-        }
+        plaintextVector[i] = temp % 26;
     }
-    decryptedMessage[messageLength] = '\0';
-    printf("Decrypted Message: %s\n", decryptedMessage);
+
+    printf("\nDecrypted plaintext: ");
+    for (i = 0; i < 3; i++) {
+        printf("%c", plaintextVector[i] + 'A');
+    }
 
     return 0;
 }
